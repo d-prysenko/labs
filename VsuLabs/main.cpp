e64a6d97ca5f0b34ffca44d647ee5645e4bf40c4
Prysenko Dmitiy <d.prysenko@ya.ru>
2022-05-12 02:04:01 +0300

    complited

--- VsuLabs/main.cpp
+++ VsuLabs/main.cpp
@@ -1,15 +1,28 @@
 #include "Trie.h"
-
+#include <algorithm>
+#include <list>
 int main()
 {
-    TrieTree T;
-    string w = " ";
-    string words[] = { "we" ,"walk" ,"want","wish","wit","am", "yo" ,"will", "wee" ,"war" ,"warp" ,"win" };
-    for (int i = 0; i < 12; i++)
-        T.Insert(words[i]);
-    T.printWord(w);
-    for (int i = 0; i < 12; i++)
-        T.Delete(words[i]);
-    cout << "\n=========\n";
-    T.printWord(w);
+	TrieTree T;
+	string w = " ";
+	string words[] = { "we" ,"walk" ,"want","wa","wish","wit","am", "yo" ,"will", "wee" ,"war" ,"warp" ,"win" };
+
+	for (int i = 0; i < 12; i++)
+		T.Insert(words[i]);
+
+	T.ForEach([](string value) { cout << value << endl; });
+
+	cout << "\n=========\n";
+
+	T.EraseIf([](string value) {
+		return value.length() % 2 == 0;
+	});
+		
+	T.ForEach([&T](string value) { cout << value << endl; });
+
+	cout << "\n=========\n";
+
+	T.printWord(w);
+
+	system("pause");
 }
