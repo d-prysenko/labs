12698d07bac5720f60410aaa1a41186402020a8f
Prysenko Dmitiy <d.prysenko@ya.ru>
2022-09-09 05:52:39 +0300

    complited

---
+++ VsuLabs/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+// Backtracking
+
+// 3. Ќапишите программу, котора€ выводит все перестановки длины N такие,
+// что любые два соседних элемента отличаютс€ как минимум на два.
+// Ќапример, если N = 4, то это 1324 и 2413.
+
+void print_description()
+{
+	printf("3. Ќапишите программу, котора€ выводит все перестановки длины N такие,\n");
+	printf("что любые два соседних элемента отличаютс€ как минимум на два.\n");
+	printf("Ќапример, если N = 4, то это 1324 и 2413.\n\n");
+}
+
+void get_transposition(std::vector<int> start, std::list<int> free_numbers)
+{
+	if (free_numbers.size() == 0)
+	{
+		std::string res;
+
+		for (const int& val : start)
+		{
+			res += std::to_string(val) + " ";
+		}
+
+		printf("%s\n", res.c_str());
+		return;
+	}
+
+	for (const int& free_num : free_numbers)
+	{	
+		if (start.size() && abs(free_num - start.back()) < 2)
+			continue;
+
+		std::vector<int> new_start = start;
+		new_start.push_back(free_num);
+
+		std::list<int> new_free_numbers = free_numbers;
+		new_free_numbers.remove(free_num);
+
+		get_transposition(new_start, new_free_numbers);
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	print_description();
+
+	int length;
+
+	printf("¬ведите длину последовательности: ");
+	std::cin >> length;
+
+	std::list<int> numbers;
+	for (int i = 1; i <= length; ++i)
+		numbers.push_back(i);
+
+	get_transposition({}, numbers);
+
+	system("pause");
+	return 0;
+}
