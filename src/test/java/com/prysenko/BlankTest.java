bee3a43a4d57b4f81a1dd938cef309bd4020d9d1
d.prysenko <prototip5250@gmail.com>
2022-12-15 16:56:36 +0300

    junit

---
+++ src/test/java/com/prysenko/BlankTest.java
@@ -0,0 +1,12 @@
+package com.prysenko;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BlankTest {
+    @Test
+    public void testJunitInstallation() {
+        assertEquals(1, 1);
+    }
+}
