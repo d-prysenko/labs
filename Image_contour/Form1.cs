a138cbad9728bc85d9498b9152af99b5a44864dc
Prysenko Dmitiy <d.prysenko@ya.ru>
2023-01-21 10:56:25 +0300

    Refactor, SobelOperator

--- Image_contour/Form1.cs
+++ Image_contour/Form1.cs
@@ -18,38 +18,33 @@ namespace Image_contour
 {
     public partial class Form1 : Form
     {
-        Bitmap bitmap;
-        Bitmap bitmap2;
-        Image file; 
+        private ImageManager imageManager;
+
         public Form1()
         {
             InitializeComponent();
+            imageManager = new ImageManager();
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            //обработчик исключений
-            try
-            {
+
                 // записываем в эту переменную результат общения пользователя с openFileDialog1
                 DialogResult res = openFileDialog1.ShowDialog();
-                
-                if(res == DialogResult.OK)// пользователь выбрал файл
+
+                if (res == DialogResult.OK)// пользователь выбрал файл
                 {
-                    file = Image.FromFile(openFileDialog1.FileName);
-                    bitmap = new Bitmap(openFileDialog1.FileName);
+                    Image file = Image.FromFile(openFileDialog1.FileName);
                     pictureBox1.Image = file;
+
+                    imageManager.SetBitmap(new Bitmap(file));
                 }
                 else
                 {
                     MessageBox.Show("Файл не выбран", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
 
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);   
-            }
+
         }
 
         private void laplaceToolStripMenuItem_Click(object sender, EventArgs e)
@@ -63,79 +58,9 @@ namespace Image_contour
 
         private void sobelToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
-            try
-            {
-
-                int[,] Gx = new int[3, 3]{ {-1, 0, 1 },
-                                       {-2, 0, 2 } ,
-                                       {-1, 0, 1 } };
-
-                int[,] Gy = new int[3, 3]{ {-1,-2,-1},
-                                       { 0, 0, 0 },
-                                       { 1, 2, 1}};
-
-                int[,] TotalGx = new int[bitmap.Width, bitmap.Height];
-                int[,] TotalGy = new int[bitmap.Width, bitmap.Height];
-                int[,] GTotal = new int[bitmap.Width, bitmap.Height];
-
-                bitmap2 = new Bitmap(bitmap.Width, bitmap.Height);
-
-                for (int i = 1; i < bitmap.Width - 1; i++)
-                {
-                    for (int j = 1; j < bitmap.Height - 1; j++)
-                    {
-                        Color a = bitmap.GetPixel(i - 1, j - 1);//[0][0]
-                        Color b = bitmap.GetPixel(i - 1, j);    //[0][1]
-                        Color c = bitmap.GetPixel(i - 1, j + 1);//[0][2]
-
-                        Color d = bitmap.GetPixel(i, j - 1);    //[1][0]
-                        Color f = bitmap.GetPixel(i, j);        //[1][1]
-                        Color g = bitmap.GetPixel(i, j + 1);    //[1][2]
-
-                        Color h = bitmap.GetPixel(i + 1, j - 1);//[2][0]
-                        Color l = bitmap.GetPixel(i + 1, j);    //[2][1]
-                        Color n = bitmap.GetPixel(i + 1, j + 1); //[2][2]
-
-                        TotalGx[i, j] = Gx[0, 0] * a.G + Gx[0, 1] * b.G + Gx[0, 2] * c.G
-                                       + Gx[1, 0] * d.G + Gx[1, 1] * f.G + Gx[1, 2] * g.G
-                                       + Gx[2, 0] * h.G + Gx[2, 1] * l.G + Gx[2, 2] * n.G;
-
-                        if (TotalGx[i, j] < 0) { TotalGx[i, j] = 0; }
-                        if (TotalGx[i, j] > 255) { TotalGx[i, j] = 255; }
-
-                        TotalGy[i, j] = Gy[0, 0] * a.G + Gy[0, 1] * b.G + Gy[0, 2] * c.G
-                                      + Gy[1, 0] * d.G + Gy[1, 1] * f.G + Gy[1, 2] * g.G
-                                      + Gy[2, 0] * h.G + Gy[2, 1] * l.G + Gy[2, 2] * n.G;
-
-                        if (TotalGy[i, j] < 0) { TotalGy[i, j] = 0; }
-                        if (TotalGy[i, j] > 255) { TotalGy[i, j] = 255; }
-
-                    }
-
-                    for (int k = 1; k < bitmap.Width - 1; k++)
-                    {
-                        for (int j = 1; j < bitmap.Height - 1; j++)
-                        {
-                            GTotal[k, j] = TotalGx[k, j] + TotalGy[k, j];
-
-                            if (GTotal[k, j] >= 255)
-                            { GTotal[k, j] = 255; }
-
-                            if (GTotal[k, j] < 0)
-                            { GTotal[k, j] = 0; }
-
-                            bitmap2.SetPixel(k, j, Color.FromArgb(GTotal[k, j], GTotal[k, j], GTotal[k, j]));
-                        }
-                    }
-                    pictureBox2.Image = bitmap2;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+             pictureBox2.Image = imageManager.SobelOperator();
         }
+
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
             saveFileDialog1.Filter = "Файл PNG (*.png)|*.png|Все файлы (*.*)|*.*";
@@ -145,10 +70,10 @@ namespace Image_contour
             }
         }
 
-      
+
     }
 
-       
-      
-  
+
+
+
 }
---
+++ Image_contour/ImageManager.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Image_contour
+{
+    class ImageManager
+    {
+        private Bitmap _bitmap;
+
+        public ImageManager()
+        { }
+
+        public ImageManager(Bitmap bitmap) => SetBitmap(bitmap);
+
+        public ImageManager SetBitmap(Bitmap bitmap)
+        {
+            _bitmap = bitmap;
+
+            return this;
+        }
+
+        public Bitmap GetBitmap()
+        {
+            return _bitmap;
+        }
+
+        public Bitmap LaplaceOperator()
+        {
+            Bitmap res = null;
+
+            return res;
+        }
+
+        public Bitmap RobertsOperator()
+        {
+            Bitmap res = null;
+
+            return res;
+        }
+
+        public Bitmap SobelOperator()
+        {
+            Bitmap res;
+
+            int width = _bitmap.Width;
+            int height = _bitmap.Height;
+
+            res = new Bitmap(width, height);
+
+            for (int i = 1; i < height - 1; i++)
+            {
+                for (int j = 1; j < width - 1; j++)
+                {
+                    int currentPixel = ApplySobelOperator(j, i);
+                    res.SetPixel(j, i, Color.FromArgb(currentPixel, currentPixel, currentPixel));
+                }
+            }
+
+            return res;
+        }
+
+
+        private int ApplySobelOperator(int x, int y)
+        {
+            int res;
+
+            int[,] Gx = new int[3, 3]{ {-1, 0, 1 },
+                                       {-2, 0, 2 } ,
+                                       {-1, 0, 1 } };
+
+            int[,] Gy = new int[3, 3]{ {-1,-2,-1},
+                                       { 0, 0, 0 },
+                                       { 1, 2, 1}};
+
+            res = MultiplyMatrixByBitmapSquare(Gx, x, y) + MultiplyMatrixByBitmapSquare(Gy, x, y);
+
+            res = Math.Min(res, 255);
+            res = Math.Max(res, 0);
+
+            return res;
+        }
+
+        private int MultiplyMatrixByBitmapSquare(int[,] mat3x3, int x, int y)
+        {
+            int res = 0;
+
+            for (int i = 0; i < 3; ++i)
+            {
+                Color a = _bitmap.GetPixel(x - 1, y + i - 1);
+                Color b = _bitmap.GetPixel(x,     y + i - 1);
+                Color c = _bitmap.GetPixel(x + 1, y + i - 1);
+
+                res += mat3x3[i, 0] * a.G + mat3x3[i, 1] * b.G + mat3x3[i, 2] * c.G;
+            }
+
+            res = Math.Min(res, 255);
+            res = Math.Max(res, 0);
+
+            return res;
+        }
+    }
+}
