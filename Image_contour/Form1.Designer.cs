4ef28760c4371d940fe722edac80e678a0ab1a5e
Prysenko Dmitiy <d.prysenko@ya.ru>
2023-01-21 09:29:42 +0300

    Initial commit

---
+++ Image_contour/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Image_contour
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.laplaceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.robertsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.sobelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.filterToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(904, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // filterToolStripMenuItem
+            // 
+            this.filterToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.laplaceToolStripMenuItem,
+            this.robertsToolStripMenuItem,
+            this.sobelToolStripMenuItem});
+            this.filterToolStripMenuItem.Name = "filterToolStripMenuItem";
+            this.filterToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.filterToolStripMenuItem.Text = "Filter";
+            // 
+            // laplaceToolStripMenuItem
+            // 
+            this.laplaceToolStripMenuItem.Name = "laplaceToolStripMenuItem";
+            this.laplaceToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.laplaceToolStripMenuItem.Text = "Laplace";
+            this.laplaceToolStripMenuItem.Click += new System.EventHandler(this.laplaceToolStripMenuItem_Click);
+            // 
+            // robertsToolStripMenuItem
+            // 
+            this.robertsToolStripMenuItem.Name = "robertsToolStripMenuItem";
+            this.robertsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.robertsToolStripMenuItem.Text = "Roberts";
+            this.robertsToolStripMenuItem.Click += new System.EventHandler(this.robertsToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(413, 424);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(479, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(413, 424);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPG|*.jpg|PNG|*.png";
+            // 
+            // sobelToolStripMenuItem
+            // 
+            this.sobelToolStripMenuItem.Name = "sobelToolStripMenuItem";
+            this.sobelToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sobelToolStripMenuItem.Text = "Sobel";
+            this.sobelToolStripMenuItem.Click += new System.EventHandler(this.sobelToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 472);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Image";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem laplaceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem robertsToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem sobelToolStripMenuItem;
+    }
+}
+
---
+++ Image_contour/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace Image_contour
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bitmap;
+        Bitmap bitmap2;
+        Image file; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //обработчик исключений
+            try
+            {
+                // записываем в эту переменную результат общения пользователя с openFileDialog1
+                DialogResult res = openFileDialog1.ShowDialog();
+                
+                if(res == DialogResult.OK)// пользователь выбрал файл
+                {
+                    file = Image.FromFile(openFileDialog1.FileName);
+                    bitmap = new Bitmap(openFileDialog1.FileName);
+                    pictureBox1.Image = file;
+                }
+                else
+                {
+                    MessageBox.Show("Файл не выбран", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);   
+            }
+        }
+
+        private void laplaceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void robertsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sobelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                int[,] Gx = new int[3, 3]{ {-1, 0, 1 },
+                                       {-2, 0, 2 } ,
+                                       {-1, 0, 1 } };
+
+                int[,] Gy = new int[3, 3]{ {-1,-2,-1},
+                                       { 0, 0, 0 },
+                                       { 1, 2, 1}};
+
+                int[,] TotalGx = new int[bitmap.Width, bitmap.Height];
+                int[,] TotalGy = new int[bitmap.Width, bitmap.Height];
+                int[,] GTotal = new int[bitmap.Width, bitmap.Height];
+
+                bitmap2 = new Bitmap(bitmap.Width, bitmap.Height);
+
+                for (int i = 1; i < bitmap.Width - 1; i++)
+                {
+                    for (int j = 1; j < bitmap.Height - 1; j++)
+                    {
+                        Color a = bitmap.GetPixel(i - 1, j - 1);//[0][0]
+                        Color b = bitmap.GetPixel(i - 1, j);    //[0][1]
+                        Color c = bitmap.GetPixel(i - 1, j + 1);//[0][2]
+
+                        Color d = bitmap.GetPixel(i, j - 1);    //[1][0]
+                        Color f = bitmap.GetPixel(i, j);        //[1][1]
+                        Color g = bitmap.GetPixel(i, j + 1);    //[1][2]
+
+                        Color h = bitmap.GetPixel(i + 1, j - 1);//[2][0]
+                        Color l = bitmap.GetPixel(i + 1, j);    //[2][1]
+                        Color n = bitmap.GetPixel(i + 1, j + 1); //[2][2]
+
+                        TotalGx[i, j] = Gx[0, 0] * a.G + Gx[0, 1] * b.G + Gx[0, 2] * c.G
+                                       + Gx[1, 0] * d.G + Gx[1, 1] * f.G + Gx[1, 2] * g.G
+                                       + Gx[2, 0] * h.G + Gx[2, 1] * l.G + Gx[2, 2] * n.G;
+
+                        if (TotalGx[i, j] < 0) { TotalGx[i, j] = 0; }
+                        if (TotalGx[i, j] > 255) { TotalGx[i, j] = 255; }
+
+                        TotalGy[i, j] = Gy[0, 0] * a.G + Gy[0, 1] * b.G + Gy[0, 2] * c.G
+                                      + Gy[1, 0] * d.G + Gy[1, 1] * f.G + Gy[1, 2] * g.G
+                                      + Gy[2, 0] * h.G + Gy[2, 1] * l.G + Gy[2, 2] * n.G;
+
+                        if (TotalGy[i, j] < 0) { TotalGy[i, j] = 0; }
+                        if (TotalGy[i, j] > 255) { TotalGy[i, j] = 255; }
+
+                    }
+
+                    for (int k = 1; k < bitmap.Width - 1; k++)
+                    {
+                        for (int j = 1; j < bitmap.Height - 1; j++)
+                        {
+                            GTotal[k, j] = TotalGx[k, j] + TotalGy[k, j];
+
+                            if (GTotal[k, j] >= 255)
+                            { GTotal[k, j] = 255; }
+
+                            if (GTotal[k, j] < 0)
+                            { GTotal[k, j] = 0; }
+
+                            bitmap2.SetPixel(k, j, Color.FromArgb(GTotal[k, j], GTotal[k, j], GTotal[k, j]));
+                        }
+                    }
+                    pictureBox2.Image = bitmap2;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Файл PNG (*.png)|*.png|Все файлы (*.*)|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox2.Image.Save(saveFileDialog1.FileName);
+            }
+        }
+
+      
+    }
+
+       
+      
+  
+}
